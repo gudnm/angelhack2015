501ae41b8c1043846525b19eb2d4378268e1021c
gudnm <srochno@gmail.com>
2015-07-12 02:45:34 +0000

    Remove host specification from server code and add codes read with receiver

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var fs = require("fs"),
 	express = require("express");
 
 var config = JSON.parse(fs.readFileSync("server_config.json"));
-var port = config.port, host = config.host;
+var port = config.port;
 var app = express();
 
 app.get('/index', function(request, response) {
 	response.send("Hello!");
 	console.log(request.query.command);
 });
-
-app.listen(port, host);
+console.log("listening on port:"+port);
+app.listen(port);
