fd402444a4bcca467454d249ed0c7c3299490d88
gudnm <srochno@gmail.com>
2015-07-12 07:44:45 +0000

    Add IR blasting

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var fs = require("fs"), 
-	express = require("express");
+express = require("express");
 
 var config = JSON.parse(fs.readFileSync("server_config.json"));
 var port = config.port;
 var app = express();
 
 app.get('/index', function(request, response) {
-	response.send("Hello!");
-	console.log(request.query.command);
+        var cmd = request.query.key;
+	console.log("Command Received " + cmd);
+
+        var signal = "irsend SEND_ONCE mediasonic " + cmd; 
+        console.log("Executing : " + signal);
+        
+        exec(signal, function(error, stdout, stderr){
+        if(error)
+            response.send("Error sending command "+ cmd);
+        else
+            response.send("Successfully sent command " + cmd); 
+   });
 });
-console.log("listening on port:"+port);
+console.log("listening on port: "+port);
 app.listen(port);
